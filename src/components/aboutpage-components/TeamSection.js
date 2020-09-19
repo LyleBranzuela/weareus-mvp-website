5c200c2c6cdb6b496950c15b749dfd78c560b934
nigelph <54135755+nigelph@users.noreply.github.com>
2020-09-20 03:36:44 +1200

    update
    removed old code

--- src/components/aboutpage-components/TeamSection.js
+++ src/components/aboutpage-components/TeamSection.js
@@ -1,66 +1,5 @@
 import React from "react";
 import "./TeamSection.css";
-<<<<<<< Updated upstream
-function TeamSection() {
-  return (
-    <div>
-      <div className="container-fluid bottom-padding teamSectionStyle aboutFluidContainer">
-        <div className="row">
-          <div className="col">
-            <img
-              src={require("../../assets/images/weareus_team/team_dan_matthews.jpg")}
-              alt="dan-image"
-              id="emp-frame"
-            />
-            <h4 id="name">Dan Matthews</h4>
-            <h4 id="role">Creative Director</h4>
-
-            <p id="team-description">
-              We are Us is such a unique platform that I needed some special
-              talents around. And Dan fits that brilliantly. He brings to We are
-              Us international experience with branding projects including with
-              My Food Bag, Te Uru and The Great Catering Company. Dan's also
-              managed large-scale brand and design projects for Auckland
-              Airport, New Zealand Trade and Enterprise and Te Wānanga o
-              Aotearoa.
-              <br />
-              <br />
-              What does he do with all that experience? He makes sure that
-              everything We are Us does connects with both health, wellness &
-              self-improvement professionals and their clients. So your business
-              can be presented in the best possible way.
-            </p>
-            <a href="https://designcabin.co.nz">designcabin.co.nz</a>
-          </div>
-          <div className="col">
-            <img
-              src={require("../../assets/images/weareus_team/team_walter_rumbsy.jpg")}
-              alt="walter-image"
-              id="emp-frame"
-            />
-            <h4 id="name">Walter Rumbsy</h4>
-            <h4 id="role">Creative Director</h4>
-            <p id="team-description">
-              With all things online I needed a web developer that could support
-              the incredible dreams I have for We are Us. I want to provide you
-              with a service that's reliable, responsive and does what it says
-              on the packet.
-              <br />
-              <br />
-              With a background with the big-names (Xero, Orion Health and My
-              Food Bag) as a lead developer, head of engineering and front-end
-              practice lead, Walt's our man. Most of what Walt does is totally
-              beyond me but I do know that Walt makes all the behind-the-scenes
-              code work to ensure that your experience is top notch and seamless
-              for both practitioners and users.
-            </p>
-          </div>
-        </div>
-        {/* Row 2 */}
-        <div className="row">
-          <div className="col">
-            {/* Uncomment code below with appropriate jpg file name to produce circular employee image */}
-=======
 import { Container, Col, Row } from "react-bootstrap";
 
 class TeamSection extends React.Component {
@@ -98,7 +37,6 @@ class TeamSection extends React.Component {
                 </p>
                 <a href="https://designcabin.co.nz">designcabin.co.nz</a>
             </Col>
->>>>>>> Stashed changes
 
             <Col sm={4} id="team-column-padding">
               <img
